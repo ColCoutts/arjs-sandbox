d637fccbf627da6fae0d05f3934ed570065a22f3
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-17 16:03:25 -0700

    swapped out videos

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@ function playVideos() {
   vid.play();
   vid2.play()
 }
+
+// function sockMerchant(ar) {
+//   ar.sort();
+//   let previousSock = null;
+
+//   return ar.reduce((accumulator, currentSock) => {
+//       console.log(ar, 'array');
+//       console.log(currentSock, 'currentsock');
+//       console.log(previousSock, 'prevsock');
+//       if (currentSock === previousSock) {
+//           // let indexSock = ar.indexOf(currentSock);
+//           // let prevInd = ar.indexOf(previousSock);
+//           // console.log(indexSock, 'currentsock');
+//           // console.log(prevInd, 'previousSock');
+//           previousSock = "hi";
+//           accumulator += 1;
+//       }
+//       else {
+//           previousSock = currentSock;
+//       }
+//       return accumulator;
+//   }, 0)
+// }
+
+// const arrayNum = [ 10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+// console.log(sockMerchant(arrayNum));
